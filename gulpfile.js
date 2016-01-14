0e235e07af60b7f846fa8311b463d70da474a674
whosesmile <lovemoon@yeah.net>
2016-01-14 15:09:00 +0800

    refctor structor

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var path = require('path');
 var gulp = require('gulp');
-var watch = require('gulp-watch');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var sequence = require('run-sequence');
@@ -39,20 +38,26 @@ gulp.task('px2rem', function () {
 });
 
 /**
- * angular concat all files
+ * concat all css files
  */
-// gulp.task('concat', function () {
-//   return gulp.src(['src/css/**/*'])
-//     .pipe(concat('base.css'))
-//     .pipe(gulp.dest(path.join(target, 'css'))).pipe(connect.reload());
-// });
+gulp.task('concat', ['px2rem'], function () {
+  return gulp.src([
+    target + '/css/widget/reset.css',
+    target + '/css/widget/global.css',
+    target + '/css/widget/flexbox.css',
+    target + '/css/widget/space.css',
+    target + '/css/widget/text.css',
+    target + '/css/widget/**/*',
+  ]).pipe(concat('css/ui.css')).pipe(gulp.dest(target)).pipe(connect.reload());
+});
 
 /**
  * watch file change
  */
 gulp.task('watch', function () {
   gulp.watch(['src/images/**/*', 'src/*.*'], ['sync']);
-  gulp.watch(['src/css/**/*'], ['px2rem']);
+  gulp.watch(['src/css/**/*'], ['concat']);
+  gulp.watch(['src/*.*'], ['sync']);
 });
 
 /**
@@ -70,5 +75,5 @@ gulp.task('connect', function () {
  *  Default task clean temporaries directories and launch the main optimization build task
  */
 gulp.task('default', function () {
-  sequence('clean', ['sync', 'px2rem'], ['connect', 'watch']);
+  sequence('clean', ['sync', 'concat'], ['connect', 'watch']);
 });
