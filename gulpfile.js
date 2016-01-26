4a1d09b54c996d16fd5980925495557ecda7fada
whosesmile <lovemoon@yeah.net>
2016-01-26 11:44:50 +0800

    add bar and divider style

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var concat = require('gulp-concat');
 var sequence = require('run-sequence');
 var uglify = require('gulp-uglify');
 var connect = require('gulp-connect');
+var minify = require('gulp-minify-css');
 var px2rem = require('gulp-smile-px2rem');
 
 // for dist should be dist
@@ -19,28 +20,29 @@ gulp.task('clean', function () {
   }).pipe(clean());
 });
 
-/**
- * sync app to .tmp
- */
-gulp.task('sync', function () {
-  return gulp.src(['src/images/**/*', 'src/js/**/*', 'src/*.*'], {
+// sync source
+gulp.task('sync:source', function () {
+  return gulp.src(['src/images/**/*', 'src/*.*'], {
     base: 'src'
   }).pipe(gulp.dest(target)).pipe(connect.reload());
 });
 
-/**
- * px2rem css files
- */
+// sync js
+gulp.task('sync:js', function () {
+  return gulp.src(['src/js/**/*'], {
+    base: 'src'
+  }).pipe(gulp.dest(target)).pipe(connect.reload());
+});
+
+// px2rem css files
 gulp.task('px2rem', function () {
   return gulp.src(['src/css/**/*'], {
     base: 'src'
   }).pipe(px2rem()).pipe(gulp.dest(target)).pipe(connect.reload());
 });
 
-/**
- * concat all css files
- */
-gulp.task('concat', ['px2rem'], function () {
+// concat all css files
+gulp.task('concat:css', ['px2rem'], function () {
   return gulp.src([
     target + '/css/widget/reset.css',
     target + '/css/widget/global.css',
@@ -54,18 +56,30 @@ gulp.task('concat', ['px2rem'], function () {
   ]).pipe(concat('css/ui.css')).pipe(gulp.dest(target)).pipe(connect.reload());
 });
 
-/**
- * watch file change
- */
-gulp.task('watch', function () {
-  gulp.watch(['src/images/**/*', 'src/*.*'], ['sync']);
-  gulp.watch(['src/css/**/*'], ['concat']);
-  gulp.watch(['src/*.*'], ['sync']);
+// minify css
+gulp.task('minify:css', ['concat:css'], function () {
+  return gulp.src(target + '/css/**/*')
+    .pipe(minify({
+      advanced: false
+    }))
+    .pipe(gulp.dest(target + '/css'))
+    .pipe(connect.reload());
 });
 
-/**
- * connect server
- */
+// uglify js
+gulp.task('uglify:js', function () {
+  return gulp.src(['src/js/**/*'], {
+    base: 'src'
+  }).pipe(uglify()).pipe(gulp.dest(target)).pipe(connect.reload());
+});
+
+// watch file change
+gulp.task('watch', function () {
+  gulp.watch(['src/images/**/*', 'src/*.*'], ['sync:source']);
+  gulp.watch(['src/css/**/*'], ['concat:css']);
+});
+
+// connect server
 gulp.task('connect', function () {
   connect.server({
     root: [target, '.'],
@@ -74,9 +88,13 @@ gulp.task('connect', function () {
   });
 });
 
-/**
- *  Default task clean temporaries directories and launch the main optimization build task
- */
+// Default task clean temporaries directories and launch the main optimization build task
 gulp.task('default', function () {
-  sequence('clean', ['sync', 'concat'], ['connect', 'watch']);
+  sequence('clean', ['sync:source', 'sync:js', 'concat:css'], ['connect', 'watch']);
+});
+
+// build project
+gulp.task('dist', function () {
+  target = 'dist/';
+  sequence('clean', ['sync:source', 'concat:css', 'uglify:js'], ['connect', 'watch']);
 });
